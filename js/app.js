a6e7fc79d99e38f6f02352db8963fe9fbde21272
michael <mcbrunette33@gmail.com>
2022-03-09 16:57:47 -0800

    finished lab 03

--- js/app.js
+++ js/app.js
@@ -1,56 +1,100 @@
 'use strict';
-
+let score = 0;
 let username = prompt('what is your name?');
 
-if(username){
+if (username) {
   alert('hello ' + username);
-} else{
+} else {
   alert('Hello Friend');
 }
-let answer1 = prompt('Is today a good day?').toLowerCase();
+let answer1 = prompt('Do I like the beach?').toLowerCase();
 
-if (answer1 === 'yes'){
-  alert('AWESOME');
-} else if(answer1 === 'no'){
-  alert('sorry to hear');
-}else{
+if (answer1 === 'yes' || answer1 === 'y') {
+  alert('correct, I grew up surfing');
+  score++;
+} else if (answer1 === 'no') {
+  alert('sorry your are wrong');
+} else {
   alert('I did not catch what you said');
 }
-let answer2 = prompt('Do you like food?').toLowerCase();
+let answer2 = prompt('Do I speak english?').toLowerCase();
 
-if (answer2 === 'yes'){
-  alert('ME TOO');
-} else if(answer2 === 'no'){
-  alert('you need to eat');
-}else{
-  alert('I did not catch what you said');
-} 
-let answer3 = prompt('Do you like the outdoors?').toLowerCase();
-
-if (answer3 === 'yes'){
-  alert('ME TOO');
-} else if(answer3 === 'no'){
-  alert('you need some SUN');
-}else{
+if (answer2 === 'yes' || answer1 === 'y') {
+  alert('I had to give you a freebee');
+  score++;
+} else if (answer2 === 'no') {
+  alert('come on, that was an easy one');
+} else {
   alert('I did not catch what you said');
 }
-let answer4 = prompt('Do you like the traveling?').toLowerCase();
+let answer3 = prompt('Do I travel?').toLowerCase();
 
-if (answer4 === 'yes'){
-  alert('ME TOO');
-} else if(answer4 === 'no'){
-  alert('I reccomend traveling to brazil. maybe you will change your mind');
-}else{
+if (answer3 === 'yes' || answer1 === 'y') {
+  alert('yes 24 countries and counting');
+  score++;
+} else if (answer3 === 'no') {
+  alert('you are incorrect');
+} else {
   alert('I did not catch what you said');
 }
-let answer5 = prompt('Do you like football?').toLowerCase();
+let answer4 = prompt('Do I play poker?').toLowerCase();
 
-if (answer5 === 'yes'){
-  alert('ME TOO' + username);
-} else if(answer5 === 'no'){
-  alert('Lets go Packers!!!'+ username);
-}else{
+if (answer4 === 'yes' || answer1 === 'y') {
+  alert('Almost every day, Working to win the WSOP one day');
+  score++;
+} else if (answer4 === 'no') {
+  alert('incoreect. Poker is one of my favorite things to do. I live in Las Vegas');
+} else {
+  alert('I did not catch what you said');
+}
+let answer5 = prompt('Is my favorite place to travel brazil?').toLowerCase();
+
+if (answer5 === 'yes' || answer1 === 'y') {
+  alert('you are right' + username);
+  score++;
+} else if (answer5 === 'no') {
+  alert('Sorry that is wrong ' + username);
+} else {
   alert('I did not catch what you said' + username);
 }
 
 
+let i = 0;
+let correctanswer = 11;
+while (i < 4) {
+  let answer6 = prompt('Guess a number between 1-33');
+  if (parseInt(answer6) === correctanswer) {
+    alert('wow that was easy ' + username);
+    i = 5;
+    score++;
+  } else if (parseInt(answer6) < correctanswer) {
+    alert('too low');
+    i++;
+  } else if (answer6 > correctanswer) {
+    alert('too high');
+    i++;
+  }
+  if (i === 4) {
+    alert('sorry the correct answer was 11');
+  }
+}
+let j = 0;
+let places = ["syria", "france", "germany"];
+while (j < 6) {
+  let answer7 = prompt('what my was least favorite country I have visited').toLowerCase();
+  for (let i = 0; i < places.length; i++) {
+    if (answer7 === places[i]) {
+      alert('you are correct');
+      j = 6;
+      score++;
+    }
+
+
+  }
+  j++;
+  if (j=== 6) {
+    alert('sorry your possible answers were brazil, germany and syria');
+  }
+
+}
+alert( 'your score is' + score + username);
